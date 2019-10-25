9db923de2ed52d6a0a0da89def3302313d6c76b8
SebastianSzkoluda <sebastianszkoluda@gmail.com>
2019-10-25 12:20:38 +0200

    Create Spring project

---
+++ src/main/java/com/zmp/cinema/seats/reservation/CinemaSeatsReservationApplication.java
@@ -0,0 +1,13 @@
+package com.zmp.cinema.seats.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSeatsReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSeatsReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zmp/cinema/seats/reservation/CinemaSeatsReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmp.cinema.seats.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSeatsReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
